92f792c0059a61c71ad61b4a52e3fba09297e1fa
Arun Kumar <41496801+arun14kn@users.noreply.github.com>
2025-02-22 04:10:04 -0800

    adding security group

--- main.tf
+++ main.tf
@@ -41,3 +41,23 @@ resource "aws_internet_gateway" "my_igw" {
     Name = "my-igw"
   }
 }
+
+resource "aws_security_group" "ecs_security_group" {
+  name        = "ecs_security_group"
+  description = "Allow inbound traffic for ECS tasks"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
