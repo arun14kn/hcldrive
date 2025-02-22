b7417de785dceda2fd2ff226a26de5c9789d17fa
Arun Kumar <41496801+arun14kn@users.noreply.github.com>
2025-02-22 03:56:58 -0800

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -1,1 +1,32 @@
 
+resource "aws_iam_policy" "ecs_user_policy" {
+  name        = "ecs-user-policy"
+  description = "ECS permissions for user"
+  policy      = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ecs:ListClusters",
+          "ecs:DescribeClusters",
+          "ecs:ListServices",
+          "ecs:DescribeServices",
+          "ecs:CreateCluster",
+          "ecs:DeleteCluster",
+          "ecs:RunTask",
+          "ecs:UpdateService",
+          "ecs:DeleteService"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+      {
+        Action = [
+          "iam:PassRole"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      }
+    ]
+  })
+}
